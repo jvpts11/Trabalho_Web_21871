afe90e5dfae13b24fefaf4b0651b30446c985246
jvpts11 <jvptsyt11@gmail.com>
2022-12-17 17:56:08 +0000

    IT WORKS
    YEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- Game/initscript.js
+++ Game/initscript.js
@@ -1,116 +1,128 @@
-//global variables
 let move_speed = 3;
-let gravity = 1;
-
-//related to spring dickson
-let spring_dickson = document.querySelector('.springDickson');
-let spring_bounding_box = spring_dickson.getBoundingClientRect();
-
-let background = document.querySelector('.gif_background');
-let score_val = document.querySelector('.score_val');
-let start_message = document.querySelector('.main_screen');
-let score_title = document.querySelector('score_title');
-
+    
+let gravity = 0.5;
+    
+let spring = document.querySelector('.spring');
+    
+let spring_props = spring.getBoundingClientRect();
+let background =
+    document.querySelector('.background')
+            .getBoundingClientRect();
+    
+let score_val =
+    document.querySelector('.score_val');
+let message =
+    document.querySelector('.message');
+let score_title =
+    document.querySelector('.score_title');
+    
 let game_state = 'Start';
-
-document.addEventListener('keydown', (e) =>{
-
-    if(e.key == 'Enter' && game_state != 'Play'){
-        document.querySelectorAll('.pipe').forEach((e)=>{
-            e.remove;
-        });
-        spring_dickson.style.top = '40vh';
-        game_state = 'Play';
-
-        start_message.innerHTML ='';
-
-        score_title.innerHTML = 'Jumps:';
-        score_val.innerHTML = '0';
-
-        play();
-    }
-
+    
+// Add an eventlistener for key presses
+document.addEventListener('keydown', (e) => {
+    
+  if (e.key == 'Enter' &&
+      game_state != 'Play') {
+    document.querySelectorAll('.pipe_sprite')
+              .forEach((e) => {
+      e.remove();
+    });
+    spring.style.top = '40vh';
+    game_state = 'Play';
+    message.innerHTML = '';
+    score_title.innerHTML = 'Jumps : ';
+    score_val.innerHTML = '0';
+    play();
+  }
 });
-
-function play(){
-    function move(){
-        if(game_state != 'Play') return;
-
-        let pipe = document.querySelectorAll('.pipe');
-        pipe.forEach((element) =>{
-            let pipe_bounding_box = element.getBoundingClientRect();
-            let dickson_bounding_box = spring_dickson.getBoundingClientRect();
-
-            if(pipe_bounding_box.right <= 0){
-                element.remove;
-            }
-            else{
-                if(dickson_bounding_box.left < pipe_bounding_box.left + pipe_bounding_box.width && dickson_bounding_box.left + dickson_bounding_box.width > pipe_bounding_box.left && dickson_bounding_box.top < pipe_bounding_box.top + pipe_bounding_box.height && dickson_bounding_box.top + dickson_bounding_box.height > pipe_bounding_box.top){
-                    game_state = 'End'
-                    start_message.innerHTML = 'Press Enter to Restart';
-                    start_message.style.left = '28vw';
-                    return;
-                }
-                else{
-                    if(pipe_bounding_box.right < dickson_bounding_box.left &&pipe_bounding_box.right + move_speed >= dickson_bounding_box.left && element.increase_score == '1'){
-                        score_val.innerHTML = +score_val.innerHTML + 1;
-                    }
-                    element.style.left = pipe_bounding_box.left - move_speed + 'px';
-                }
-            }
-        });
-
-        requestAnimationFrame(move);
-    }
+function play() {
+  function move() {
+      
+    if (game_state != 'Play') return;
+      
+    let pipe_sprite = document.querySelectorAll('.pipe_sprite');
+    pipe_sprite.forEach((element) => {
+        
+      let pipe_sprite_props = element.getBoundingClientRect();
+      spring_props = spring.getBoundingClientRect();
+        
+      if (pipe_sprite_props.right <= 0) {
+        element.remove();
+      } else {
+        if (
+          spring_props.left < pipe_sprite_props.left +
+          pipe_sprite_props.width &&
+          spring_props.left +
+          spring_props.width > pipe_sprite_props.left &&
+          spring_props.top < pipe_sprite_props.top +
+          pipe_sprite_props.height &&
+          spring_props.top +
+          spring_props.height > pipe_sprite_props.top
+        ) {
+            
+          // Change game state and end the game
+          // if collision occurs
+          game_state = 'End';
+          message.innerHTML = 'Press Enter To Restart';
+          message.style.left = '28vw';
+          return;
+        } else {
+          if (pipe_sprite_props.right < spring_props.left &&pipe_sprite_props.right + move_speed >= spring_props.left &&element.increase_score == '1') {
+            score_val.innerHTML = +score_val.innerHTML + 1;
+          }
+          element.style.left = 
+            pipe_sprite_props.left - move_speed + 'px';
+        }
+      }
+    });
+  
     requestAnimationFrame(move);
-
-    let spring_dy = 0;
-
-    function apply_gravity(){
-        if(game_state != 'Play') return;
-        spring_dy = spring_dy + gravity;
-        document.addEventListener('keydown', (e) =>{
-            if(e.key == 'ArrowUp' || e.key == ' '){
-                spring_dy= -7.6
-            }
-        });
-
-        if(spring_bounding_box.top <= 0 || spring_bounding_box.bottom >= background.bottom){
-            game_state = 'End';
-            start_message.innerHTML = 'Press Enter to Restart';
-            start_message.style.left = '28vw';
-            return;
-        }
-        spring_dickson.style.top = spring_bounding_box.top + spring_dy + 'px';
-        spring_bounding_box = spring_dickson.getBoundingClientRect();
-        requestAnimationFrame(apply_gravity);
+  }
+  requestAnimationFrame(move);
+  
+  let spring_dy = 0;
+  function apply_gravity() {
+    if (game_state != 'Play') return;
+    spring_dy = spring_dy + gravity;
+    document.addEventListener('keydown', (e) => {
+      if (e.key == 'ArrowUp' || e.key == ' ') {
+        spring_dy = -7.6;
+      }
+    });
+  
+    if (spring_props.top <= 0 ||
+        spring_props.bottom >= background.bottom) {
+      game_state = 'End';
+      message.innerHTML = 'Press Enter To Restart';
+      message.style.left = '28vw';
+      return;
     }
+    spring.style.top = spring_props.top + spring_dy + 'px';
+    spring_props = spring.getBoundingClientRect();
     requestAnimationFrame(apply_gravity);
+  }
+  requestAnimationFrame(apply_gravity);
+  
+  let pipe_seperation = 0;
+    
+  let pipe_gap = 35;
+  function create_pipe() {
+    if (game_state != 'Play') return;
 
-    let pipe_space = 0;
-    let magnet_space = 0;
-
-    let pipe_gap = 35;
-    let magnet_gap = 70;
-
-    function create_pipe(){
-        if(game_state != 'Play') return;
-
-        if(pipe_space > 115){
-            pipe_space = 0;
-
-            let pipe_pos = Math.floor(Math.random() * 43 + 8);
-
-            let pipe_sprite = document.createElement('div');
-            pipe_sprite.className = 'pipe_sprite';
-            pipe_sprite.style.top = pipe_pos + pipe_gap + 'vh';
-            pipe_sprite.style.left = '100vw';
-            pipe_sprite.increase_score = '1';
-
-            document.body.appendChild(pipe_sprite);
-        }
-        pipe_space++;
-        requestAnimationFrame(create_pipe);
+    if (pipe_seperation > 115) {
+      pipe_seperation = 0
+        
+      let pipe_posi = Math.floor(Math.random() * 43) + 8;
+      let pipe_sprite = document.createElement('div');
+      pipe_sprite.className = 'pipe_sprite';
+      pipe_sprite.style.top = pipe_posi + pipe_gap + 'vh';
+      pipe_sprite.style.left = '100vw';
+      pipe_sprite.increase_score = '1';
+        
+      document.body.appendChild(pipe_sprite);
     }
+    pipe_seperation++;
     requestAnimationFrame(create_pipe);
+  }
+  requestAnimationFrame(create_pipe);
 }

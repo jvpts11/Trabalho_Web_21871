a2e31a947d74d42794a9644d2299eaf836453f20
jvpts11 <jvptsyt11@gmail.com>
2022-12-17 01:25:44 +0000

    something is now being done
    at least

--- Game/initscript.js
+++ Game/initscript.js
@@ -1,1 +1,9 @@
-alert("Test")
+let move_speed = 3;
+
+let gravity = 1;
+
+let spring_dickson = document.querySelector('.springDickson');
+
+let bird_bounding_box = spring_dickson.getBoundingClientRect();
+
+let background = document.querySelector('.gif_background');

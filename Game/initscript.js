e4fdfa0f85c41b78ba32470e8d297f3d6989dc67
Dário Ribeiro <ninjago00700@gmail.com>
2023-01-18 15:13:59 +0000

    Switched code to React

--- Game/initscript.js
+++ Game/initscript.js
@@ -1,110 +1,106 @@
-let move_speed = 4;
+import React, { useState, useEffect } from "react";
 
-let gravity = 1;
+const Game = () => {
+  const [moveSpeed, setMoveSpeed] = useState(4);
+  const [gravity, setGravity] = useState(1);
+  const [gameState, setGameState] = useState("Start");
+  const [score, setScore] = useState(0);
 
-let spring = document.querySelector(".spring");
+  const springRef = React.createRef();
+  const backgroundRef = React.createRef();
 
-let spring_props = spring.getBoundingClientRect();
-let background = document.querySelector(".background").getBoundingClientRect();
-
-let score_val = document.querySelector(".score_val");
-let message = document.querySelector(".message");
-let score_title = document.querySelector(".score_title");
-
-let game_state = "Start";
-
-// Add an eventlistener for key presses
-document.addEventListener("keydown", (e) => {
-  if (e.key == "Enter" && game_state != "Play") {
-    document.querySelectorAll(".pipe_sprite").forEach((e) => {
-      e.remove();
+  useEffect(() => 
+  {
+    // Add an eventlistener for key presses
+    document.addEventListener("keydown", (e) => 
+    {
+      if (e.key === "Enter" && gameState !== "Play") 
+      {
+        setGameState("Play");
+        setScore(0);
+        play();
+      }
     });
-    spring.style.top = "40vh";
-    game_state = "Play";
-    message.innerHTML = "";
-    score_title.innerHTML = "Jumps_Score : ";
-    score_val.innerHTML = "0";
-    play();
-  }
-});
-function play() {
-  function move() {
-    if (game_state != "Play") return;
+  }, []);
 
-    let pipe_sprite = document.querySelectorAll(".pipe_sprite");
-    pipe_sprite.forEach((element) => {
-      let pipe_sprite_props = element.getBoundingClientRect();
-      spring_props = spring.getBoundingClientRect();
+  const play = () => 
+  {
+    function move() 
+    {
+      if (gameState !== "Play") return;
 
-      if (pipe_sprite_props.right <= 0) {
-        element.remove();
-      } else {
-        if (
-          spring_props.left <
-            pipe_sprite_props.left + pipe_sprite_props.width &&
-          spring_props.left + spring_props.width > pipe_sprite_props.left &&
-          spring_props.top < pipe_sprite_props.top + pipe_sprite_props.height &&
-          spring_props.top + spring_props.height > pipe_sprite_props.top
-        ) {
-          // Change game state and end the game
-          // if collision occurs
-          game_state = "End";
-          message.innerHTML = "Press Enter To Restart";
-          message.style.left = "28vw";
-          return;
-        } else {
-          element.style.left = pipe_sprite_props.left - move_speed + "px";
+      const pipeSprites = document.querySelectorAll(".pipe_sprite");
+      pipeSprites.forEach((element) => {
+        const pipeSpriteProps = element.getBoundingClientRect();
+        const springProps = springRef.current.getBoundingClientRect();
+
+        if (pipeSpriteProps.right <= 0) 
+        {
+          element.remove();
+        } else 
+        {
+          if (springProps.left < pipeSpriteProps.left + pipeSpriteProps.width && springProps.left + springProps.width > pipeSpriteProps.left && springProps.top < pipeSpriteProps.top + pipeSpriteProps.height && springProps.top + springProps.height > pipeSpriteProps.top) 
+          {
+            // Change game state and end the game, if collision occurs.
+            setGameState("End");
+            return;
+          } else 
+          {
+            element.style.left = pipeSpriteProps.left - moveSpeed + "px";
+          }
         }
-      }
-    });
+      });
 
+      requestAnimationFrame(move);
+    }
     requestAnimationFrame(move);
-  }
-  requestAnimationFrame(move);
 
-  let spring_dy = 0;
-  function apply_gravity() {
-    if (game_state != "Play") return;
-    spring_dy = spring_dy + gravity;
-    document.addEventListener("keydown", (e) => {
-      if (e.key == "ArrowUp" || e.key == " ") {
-        score_val.innerHTML++;
-        spring_dy = -7.6;
+    let springDy = 0;
+    function applyGravity() 
+    {
+      if (gameState !== "Play") return;
+      springDy = springDy + gravity;
+      document.addEventListener("keydown", (e) => 
+      {
+        if (e.key == " ") {
+          setScore(score + 1);
+          springDy = -7.6;
+        }
+      });
+
+      if (springProps.top <= 0 || springProps.bottom >= backgroundRef.current.getBoundingClientRect().bottom) 
+      {
+        setGameState("End");
+        return;
       }
-    });
-
-    if (spring_props.top <= 0 || spring_props.bottom >= background.bottom) {
-      game_state = "End";
-      message.innerHTML = "Press Enter To Restart";
-      message.style.left = "28vw";
-      return;
+      springRef.current.style.top = springProps.top + springDy + "px";
+      requestAnimationFrame(applyGravity);
     }
-    spring.style.top = spring_props.top + spring_dy + "px";
-    spring_props = spring.getBoundingClientRect();
-    requestAnimationFrame(apply_gravity);
-  }
-  requestAnimationFrame(apply_gravity);
+    requestAnimationFrame(applyGravity);
 
-  let pipe_seperation = 0;
+    let pipeSeperation = 0;
 
-  let pipe_gap = 25;
-  function create_pipe() {
-    if (game_state != "Play") return;
+    let pipeGap = 25;
+    function createPipe() 
+    {
+      if (gameState !== "Play") return;
 
-    if (pipe_seperation > 115) {
-      pipe_seperation = 0;
-
-      let pipe_posi = Math.floor(Math.random() * 43) + 8;
-      let pipe_sprite = document.createElement("div");
-      pipe_sprite.className = "pipe_sprite";
-      pipe_sprite.style.top = pipe_posi + pipe_gap + "vh";
-      pipe_sprite.style.left = "100vw";
-      pipe_sprite.increase_score = "1";
-
-      document.body.appendChild(pipe_sprite);
+      if (pipeSeperation > 115) 
+      {
+        pipeSeperation = 0;
+  
+        let pipePosi = Math.floor(Math.random() * 43) + 8;
+        let pipeSprite = document.createElement("div");
+        pipeSprite.className = "pipe_sprite";
+        pipeSprite.style.top = pipePosi + pipeGap + "vh";
+        pipeSprite.style.left = "100vw";
+       // pipeSprite.incre...
+        document.querySelector(".background").appendChild(pipeSprite);
+      } else {
+        pipeSeperation++;
+      }
+      requestAnimationFrame(createPipe);
     }
-    pipe_seperation++;
-    requestAnimationFrame(create_pipe);
+    requestAnimationFrame(createPipe);
   }
-  requestAnimationFrame(create_pipe);
 }

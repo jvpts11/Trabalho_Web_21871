a527667a433fd00c7db545784a60c91f3ceb4aa9
jvpts11 <jvptsyt11@gmail.com>
2023-01-22 10:52:44 +0000

    progress

--- spring-dickson/src/App.js
+++ spring-dickson/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Spring from './components/spring'
+import Spring from './components/Spring'
 import Background from './components/background';
 
 function App() {
   return (
-    <div className="App">
-      <Spring></Spring>
-      <Background></Background>
+    <div className="game">
+      <Background className='fundo'></Background>
+      <Spring className='SpringDickson'></Spring>
     </div>
   );
 }
